5ade5741f1c4477dcfadafe7431fe027d54949b7
Kogetsuki <rozenbastien@gmail.com>
2025-11-29 10:07:16 +0100

    grpahql + apolloClient setup

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'react-native';
 import { NativeRouter } from 'react-router-native'
+import { ApolloProvider } from '@apollo/client/react'
 
-import Main from './src/components/Main';
+import createApolloClient from './src/utils/apolloClient'
+import Main from './src/components/Main'
+
+
+const apolloClient = createApolloClient()
 
 const App = () => {
   return (
     <>
       <NativeRouter>
-        <Main />
+        <ApolloProvider client={apolloClient}>
+          <Main />
+        </ApolloProvider>
       </NativeRouter>
       <StatusBar style='auto' />
     </>
